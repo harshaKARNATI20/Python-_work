1dd2553d5ef649f6a9863e0bfd500440fa818039
harshaKARNATI20 <114800403+harshaKARNATI20@users.noreply.github.com>
2022-11-19 23:03:50 +0530

    Extended Lists

---
+++ listextend.py
@@ -0,0 +1,18 @@
+list1 = ["x","y","z"]
+list2 = [1,2,3]
+
+# for x in list2:
+    # list1.append(x)
+# print(list1)
+
+list1.extend(list2)
+print(list1)
+
+
+
+
+
+# list2.append(list1)
+# print(list2)
+
+
