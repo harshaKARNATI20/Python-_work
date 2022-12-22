1dbba16cf52ae3eadf62bbdb1226b39c56a67f3a
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-22 19:19:00 +0530

    Dict in Dict

---
+++ nestedDict.py
@@ -0,0 +1,13 @@
+myDictionary = {
+    "class":{
+        "student":{
+            "name" : "abc",
+            "marks" : {
+                "python" : 90,
+                "web" : 95
+            }
+        }
+    }
+}
+print(myDictionary["class"]["student"]["marks"]["web"])
+# just like indexing of lists,in Dictionaries,the indexes are keys i.e.,"class","student",etc..,
