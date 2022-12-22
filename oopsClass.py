a4b950fce1af3dc92cd0268a23ef5367b9851825
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-22 19:20:17 +0530

    OOP's Classes

---
+++ oopsClass.py
@@ -0,0 +1,11 @@
+# class MyClass:
+#     def __init__(self):
+#         self.my_variable = 0
+
+#     def my_method(self):
+#         try:
+#             # Do something here
+#             pass
+#         # except SomeError:
+#             # Handle the error
+#             pass
