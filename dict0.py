6d7a4fce18c8dcc5baab0dd0a2eca127135d621e
Harsha Karnati <114800403+harshaKARNATI20@users.noreply.github.com>
2022-12-22 19:18:32 +0530

    Dictionary Intro

---
+++ dict0.py
@@ -0,0 +1,43 @@
+# myDictionary = {
+#     "name": str(input()),
+#     "age":int(input()),
+#     "Roll no.":int(input()),
+#     "class":int(input()),
+#     "Percentage":float(input())
+# }
+
+# print(myDictionary)
+myDictionary = {
+    "name": "Harsha",
+    "age":20,
+    "Roll no.":44,
+    "class":12,
+    "Percentage":98
+}
+# print(myDictionary)
+# print(len(myDictionary))
+# print(myDictionary.get("age"))
+# print(myDictionary.keys())
+# print(type(myDictionary.keys()))
+# print(myDictionary.items())
+# myDictionary["age"] = 22
+# myDictionary.update("age":22)
+c = myDictionary.pop("name")
+print(myDictionary)
+
+# a = myDictionary.keys()
+# b = myDictionary.values()
+# print(a)
+# print(b)
+
+
+
+
+
+# a = myDictionary.get["name"]
+# b = myDictionary.get("name")
+# print(b)
+
+# ordered
+# duplicates are not allowed
+
